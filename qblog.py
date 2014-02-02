d875385a1ad336040bbdcb0b549d53c514cd5790
Qasim Ali <qasimcanadian@gmail.com>
2014-02-01 21:58:47 -0500

    Set up directory structure, main script skeleton

---
+++ qblog.py
@@ -0,0 +1,9 @@
+# goes through the /data folder, parsing the files and generating the proper
+# HTML files in the proper locations, as well as creating links as required.
+def gen-posts():
+	pass
+
+# goes through the /data folder, getting post titles, previews, and locations,
+# putting a preview for each post on the homepage
+def gen-homepage():
+	pass
