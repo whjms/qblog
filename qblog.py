264fb0df1d120297f7031b5e289d4a99e43d16ee
Qasim Ali <qasimcanadian@gmail.com>
2014-02-13 21:46:14 -0500

    No \n in post properties, added __str__()
    Post properties are now read using f.read().splitlines(), and so there's
    no more \ns in post properties. Also added __str__() for printing/debug
    purposes.

--- qblog.py
+++ qblog.py
@@ -7,12 +7,14 @@ class Post(object):
 	# 	post_data = (title, date, time, file_path, preview, body)
 	def parse_data(self, filename):
 		f = open(filename, 'r')
-		title = f.readline()
-		date = f.readline()
-		time = f.readline()
-		file_path = f.readline()
-		preview = f.readline()
-		body = f.read()
+		lines = f.read().splitlines()
+		title = lines[0]
+		date = lines[1]
+		time = lines[2]
+		file_path = lines[3]
+		preview = lines[4]
+		# join each item in the body, separating them with newlines
+		body = "\n".join(lines[5:])
 		f.close()
 
 		# if not (all strings are non-empty)
@@ -20,11 +22,16 @@ class Post(object):
 			print("Error parsing post file %d, exiting." (filename))
 			sys.exit(1)
 		else:
+			# use a tuple, since there's no reason to ever edit a post
 			self.post_data = (title, date, time, file_path, preview, body)
 
 	# create html file for this post
 	def gen_post_html(self):
 		pass
+
+	# string representation: all values in post_data
+	def __str__(self):
+		return "\n".join(value for value in self.post_data)
 
 	# return a specific value from post_data, given either:
 	# 	'title', 'date', 'time', 'file_path', 'preview', or 'body'
@@ -45,8 +52,8 @@ def gen_homepage():
 	pass
 
 def main():
-	p = Post(r"H:\documents\qblog\data\test-post.post")
-	print(p.get('body'))
+	p = Post(r"data\test-post.post")
+	print(p)
 
 if __name__ == "__main__":
 	main()
